61d653e7616b403d18edba97c377a9b4193e1610
Jekyllina <dianasternini@hotmail.it>
2022-02-22 16:47:00 +0100

    added test

--- test/src/dynarray_test.c
+++ test/src/dynarray_test.c
@@ -30,6 +30,14 @@ CLOVE_TEST(AppendSuccess) {
     int a = 10;
     dynarray_append(&array, &a);
 
+    CLOVE_NOT_NULL(&array);
+}
+
+CLOVE_TEST(VerifyGetLen)
+{
+    int a = 10;
+    dynarray_append(&array, &a);
+
     int elements = dynarray_len(&array);
     CLOVE_INT_EQ(1, elements);
 }
@@ -59,7 +67,7 @@ CLOVE_TEST(RemoveElement)
     CLOVE_INT_EQ(2, elements);
 }
 
-CLOVE_TEST(RemoveElementAndVerifyIfLastIsCorrect)
+CLOVE_TEST(RemoveElementAndVerifyIfOthersAreStillCorrect)
 {
     int a = 10;
     int b = 30;
@@ -68,9 +76,13 @@ CLOVE_TEST(RemoveElementAndVerifyIfLastIsCorrect)
     dynarray_append(&array, &b);
     dynarray_append(&array, &c);
 
-    dynarray_remove(&array, 2);
+    dynarray_remove(&array, 1);
 
     int value;
-    dynarray_get(&array, 1, &value);
-    CLOVE_INT_EQ(30, value);
+    dynarray_get(&array, 0, &value);
+    CLOVE_INT_EQ(10, value);
+
+    int value2;
+    dynarray_get(&array, 1, &value2);
+    CLOVE_INT_EQ(50, value2);
 }
